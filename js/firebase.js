f0b39ede0d9259d1412ddbd7b3cb031adbe7ec83
Guilherme Felix <guilher.felixrosa@gmail.com>
2022-09-11 10:53:22 -0300

    Primeiro commit

---
+++ js/firebase.js
@@ -0,0 +1,19 @@
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDIswak1lLjB6WkEVf11reHwLg_TtzzWjc",
+  
+    authDomain: "cadastro-a5105.firebaseapp.com",
+  
+    databaseURL: "https://cadastro-a5105-default-rtdb.firebaseio.com/",
+  
+    projectId: "cadastro-a5105",
+  
+    storageBucket: "cadastro-a5105.appspot.com",
+  
+    messagingSenderId: "1032965561452",
+  
+    appId: "1:1032965561452:web:2db69a48a8ade33db74c49"
+  
+  };
+firebase.initializeApp(firebaseConfig);
+  
